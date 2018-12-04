8b4fdd5652e78bae0689b9199df8d4b6b51a9320
Sirilada244Mai <sirilada.mai@gmail.com>
2018-12-04 15:31:28 +0700

    Update Invader-BEYOND.py

--- Invader-BEYOND.py
+++ Invader-BEYOND.py
@@ -20,11 +20,17 @@ black = (0, 0, 0)
 
 #player info
 ship = image.load('player.png')
-ply_width = 62
+ply_width = 93
 
 bg = image.load('background.png').convert()
 
 comet = image.load('comet.png')
+
+stk = image.load('inv03.png')
+
+
+def striker(shape, stk_x, stk_y):
+    screen.blit(shape, (stk_x, stk_y))
 
 #blit player to screen
 def play(x, y):
@@ -32,6 +38,7 @@ def play(x, y):
 
 #main game loop
 def game_loop():
+
     ply_spd = 0 
     ply_x = 350
     ply_y = 488
@@ -40,9 +47,15 @@ def game_loop():
     bg_y = 600
 
     thg_srtx = randrange(50, dis_width - 50)
-    thg_srty = -600
+    thg_srty = -300
+
+    stk_wid = 74
+    stk_spd = 1
+    stk_high = 90
+    findenemy = True
 
     while True:
+        #check keyboard input
         for evt in event.get():
             if evt.type == QUIT:
                 quit()
@@ -59,20 +72,37 @@ def game_loop():
                 if evt.key == K_RIGHT or evt.key == K_LEFT:
                     ply_spd = 0
 
+        if findenemy:
+            if thg_srtx < ply_x:
+                thg_srtx += stk_spd
+                thg_srty += stk_spd
+            elif thg_srtx > ply_x:
+                thg_srtx -= stk_spd
+                thg_srty += stk_spd
+            elif thg_srtx == ply_x or thg_srtx + stk_wid == ply_x:
+                findenemy = False
+        elif not findenemy:
+            thg_srty += 3
 
-        rel_y = (bg_y % bg.get_rect().width)
+        if thg_srty > dis_height:
+            thg_srty = 0 - stk_high
+            thg_srtx = randrange(50, dis_width - 50)
+            findenemy = True
 
         ply_x += ply_spd
         screen.fill(black)
 
+
+        rel_y = (bg_y % bg.get_rect().width)
         screen.blit(bg, (0, (rel_y - bg.get_rect().width)))
         if rel_y < dis_height:
             screen.blit(bg, (0, rel_y))
         bg_y += 1
 
+        striker(stk, thg_srtx, thg_srty)
         play(ply_x, ply_y)
 
-        if ply_x > dis_width - (ply_width+50) or ply_x < 50:
+        if ply_x > dis_width - (ply_width+40) or ply_x < 50:
             ply_spd = 0
 
             
