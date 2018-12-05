1e79fad34516fc942284951584359276ea44d2a8
PrompopKeawsod <42951458+PrompopKeawsod@users.noreply.github.com>
2018-12-05 11:36:44 +0700

    Update Invader-BEYOND.py

--- Invader-BEYOND.py
+++ Invader-BEYOND.py
@@ -20,39 +20,103 @@ black = (0, 0, 0)
 
 #player info
 ship = image.load('player.png')
-ply_width = 93
-
 bg = image.load('background.png').convert()
-
 comet = image.load('comet.png')
-
 stk = image.load('inv03.png')
+rng = image.load('inv01.png')
+
+def target(x1, w1, x2, w2):
+    if(x2+w2>=x1>=x2) or (x2+w2>=x1+w1>=x2) or (x2+w2>=x1>=x2) or (x2+w2>=x1+w1>=x2):
+        return True
+    return False
+
+def enemy(enemy_box, ply_x, ply_width, num_enemy, max_enemy, findply):
+    rand_enemy(enemy_box, num_enemy, max_enemy)
+    for i in enemy_box:
+        if i[1] == 1:
+            striker(stk, i[2], i[3], ply_x, ply_width, findply)
+        if i[1] == 2:
+            commet(comet, i[2], i[3])
+        if i[1] == 3:
+            ranger(rng, i[2], i[3])
+    for i in range(len(enemy_box)-1, -1, -1):
+        if enemy_box[i][3] > dis_height:
+            del enemy_box[i]
+
+def rand_enemy(enemy_box, num_enemy, max_enemy):
+    stk_y = -74
+    stk_x = randint(50, dis_width-(74+50))
+
+    com_y = -200
+    com_x = randint(50, dis_width-(62+50))
+
+    rng_y = 0
+    rng_x = randint(50, dis_width-(78+50))
+
+    if num_enemy < max_enemy:
+        for i in range(num_enemy, max_enemy):
+            enemy_type = randrange(1, 3)
+            if enemy_type == 1:
+                enemy_box.append([stk, 1, stk_x, stk_y])
+            elif enemy_type == 2:
+                enemy_box.append([comet, 2, com_x, com_y])
+            elif enemy_type == 3:
+                enemy_box.append([rng, 3, com_x, com_y])
+            num_enemy += 1
+        return num_enemy
+
+def commet(img, x, y):
+    wid = 62
+    spd = 3
+    high = 62
+    screen.blit(img, (x, y - spd))
+
+def ranger(img, x, y):
+    wid = 78
+    high = 68
+    spd = 1
+    if x < 50:
+        spd = 1
+    elif x > dis_width - (wid + 50):
+        spd = -1
+    screen.blit(img, (x + spd, y))
 
 
-def striker(shape, stk_x, stk_y):
-    screen.blit(shape, (stk_x, stk_y))
+def striker(img, x, y, p_x, p_w, findply):
+    wid = 74
+    spd = 5
+    high = 90
+    if findply:
+        if x > p_x:
+            x -= 2
+            y += 1
+        if x < p_x:
+            x += 2
+        elif target(x, wid, p_x, p_w):
+            findply = False
 
-#blit player to screen
-def play(x, y):
-    screen.blit(ship, (x, y))
+    else:
+        st_y += spd
+
+    screen.blit(img, (x, y))
 
 #main game loop
 def game_loop():
-
+    
+    enemy_box = []
+    num_enemy = 0
+    max_enemy = 3
+    
+    findply = True
     ply_spd = 0 
     ply_x = 350
     ply_y = 488
-
+    ply_width = 93
     rel_y = 0
     bg_y = 600
 
     thg_srtx = randrange(50, dis_width - 50)
-    thg_srty = -300
-
-    stk_wid = 74
-    stk_spd = 1
-    stk_high = 90
-    findenemy = True
+    thg_srty = 0
 
     while True:
         #check keyboard input
@@ -72,26 +136,9 @@ def game_loop():
                 if evt.key == K_RIGHT or evt.key == K_LEFT:
                     ply_spd = 0
 
-        if findenemy:
-            if thg_srtx < ply_x:
-                thg_srtx += stk_spd
-                thg_srty += stk_spd
-            elif thg_srtx > ply_x:
-                thg_srtx -= stk_spd
-                thg_srty += stk_spd
-            elif thg_srtx == ply_x or thg_srtx + stk_wid == ply_x:
-                findenemy = False
-        elif not findenemy:
-            thg_srty += 3
-
-        if thg_srty > dis_height:
-            thg_srty = 0 - stk_high
-            thg_srtx = randrange(50, dis_width - 50)
-            findenemy = True
-
         ply_x += ply_spd
         screen.fill(black)
-
+        enemy(enemy_box, ply_x, ply_width, num_enemy, max_enemy, findply)
 
         rel_y = (bg_y % bg.get_rect().width)
         screen.blit(bg, (0, (rel_y - bg.get_rect().width)))
@@ -99,8 +146,7 @@ def game_loop():
             screen.blit(bg, (0, rel_y))
         bg_y += 1
 
-        striker(stk, thg_srtx, thg_srty)
-        play(ply_x, ply_y)
+        screen.blit(ship, (ply_x, ply_y))
 
         if ply_x > dis_width - (ply_width+40) or ply_x < 50:
             ply_spd = 0
